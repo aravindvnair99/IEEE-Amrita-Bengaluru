411ce601ee7e7e00d508081b5d76f6638c2c8c86
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-10-09 11:51:12 +0530

    Add routes

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,39 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+app.get('/Apply', (req, res) => {
+	res.render('apply');
+});
+app.get('/Committee', (req, res) => {
+	res.render('committee');
+});
+app.get('/Feedback', (req, res) => {
+	res.render('feedback');
+});
+app.get('/Join', (req, res) => {
+	res.render('join');
+});
+app.get('/Mentors', (req, res) => {
+	res.render('mentors');
+});
+app.get('/Quiz', (req, res) => {
+	res.render('quiz');
+});
+app.get('/ExecutiveApplicationFirstYear', (req, res) => {
+	res.render('ExecutiveApplicationFirstYear');
+});
+app.get('/Offline', (req, res) => {
+	res.render('offline');
+});
+app.use((req, res, next) => {
+	res.status(404).render('404');
+});
+
+exports.app = functions.https.onRequest(app);
